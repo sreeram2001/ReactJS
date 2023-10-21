1a627c078144d8aa31db9ad9d8462570ab9ec67b
Sreeram S <40134790+sreeram2001@users.noreply.github.com>
2023-10-22 01:38:46 +0530

    Create App.js

---
+++ Notes/Components/App.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+
+//function names should start with capital letter
+
+function App() { 
+return( 
+  //class cannot be used instead className is used as class is a reservde keyword in javascript.
+  <div className="content">
+  <h1>Hi There </h1>
+  </div>
+</div>
+); 
+}
+
+export default App;
